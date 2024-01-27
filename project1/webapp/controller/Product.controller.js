4bb9c2d6e832ab09e24a50d58fe24764693d802f
Manish5788 <abap5788@gmail.com>
2024-01-27 15:48:05 +0000

    added product page

--- project1/webapp/controller/Product.controller.js
+++ project1/webapp/controller/Product.controller.js
@@ -10,12 +10,12 @@ sap.ui.define([
     function (Controller,History,Device) {
         "use strict";
 
-        return Controller.extend("project1.controller.Detail", {
+        return Controller.extend("project1.controller.Product", {
             onInit : function () {
 
                 var oModel = this.getOwnerComponent().getModel("myJsonData");
                 this.getView().setModel(oModel, "myModel");
-                this.getOwnerComponent().getRouter().getRoute("orderDetails").attachPatternMatched(this._onRouteMatched, this);
+                this.getOwnerComponent().getRouter().getRoute("productDetails").attachPatternMatched(this._onRouteMatched, this);
             },
             _onRouteMatched: function(oEvent) {
                 // var oSelectedItem = oEvent.getSource() ; 
@@ -23,14 +23,15 @@ sap.ui.define([
 
 			    // var sPath = oContext.getPath();
                 this._orderId = oEvent.getParameter("arguments").orderId;
-                this.getView().bindElement({path: "/orders/" + this._orderId , model: "myModel"});
+                this._productId = oEvent.getParameter("arguments").productId;
+                this.getView().bindElement({path: "/orders/" + this._orderId + "/products/" + this._productId, model: "myModel"});
             },
-            onSelectionChange: function(oEvent) {
-                var sProductId = oEvent.getSource().getBindingContext().getProperty("productId");
-                this.getOwnerComponent().getRouter()
-                    .navTo("productDetails",
-                        {orderId:this._orderId, productId: sProductId});
-            },
+            // onSelectionChange: function(oEvent) {
+            //     var sProductId = oEvent.getSource().getBindingContext().getProperty("productId");
+            //     this.getOwnerComponent().getRouter()
+            //         .navTo("productDetails",
+            //             {orderId:this._orderId, productId: sProductId});
+            // },
             onNavBack : function() {
                 var sPreviousHash = History.getInstance().getPreviousHash();
                 //The history contains a previous entry
